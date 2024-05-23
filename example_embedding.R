5a174035e680f5900270878f446c48127d21e7f5
sempwn <mike.irvine@bccdc.ca>
2024-05-22 17:16:31 -0700

    What is a basis and other background slides.

--- example_embedding.R
+++ example_embedding.R
@@ -1,4 +1,5 @@
 library(plot.matrix)
+library(tidyverse)
 
 x <- 1:4
 (z <- x %*% x)    # scalar ("inner") product (1 x 1 matrix)
@@ -57,3 +58,31 @@ softmaxX %*% t(VX)
 matrix(c(1, 0), nrow = 1) %*% softmaxX %*% t(VX)
 
 plot(VX, border = NA,key = NULL)
+
+xlabs <- c("pet", "outdoors", "food")
+ylabs <- c("dog", "cat")
+
+
+dat2 <-
+  t(QX) %>%
+  as_tibble() %>%
+  rownames_to_column("Var1") %>%
+  pivot_longer(-Var1, names_to = "Var2", values_to = "value") %>%
+  mutate(
+    Var2 = gsub("V", "", Var2)
+  ) %>%
+  left_join(tibble(Var1 = as.character(1:length(xlabs)), xlab = xlabs)) %>%
+  left_join(tibble(Var2 = as.character(1:length(ylabs)), ylab = rev(ylabs))) %>%
+  mutate(
+    Var1 = factor(xlab, levels = xlabs),
+    Var2 = factor(ylab, levels = rev(ylabs))
+  )
+
+
+ggplot(dat2, aes(Var1, Var2)) +
+  geom_tile(aes(fill = value)) +
+  geom_text(aes(label = round(value, 1))) +
+  scale_fill_gradient2() +
+  ggplot2::theme_minimal() +
+  labs(x="",y="") +
+  theme(legend.position = "none")
